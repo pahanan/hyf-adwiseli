8c71d85a58571c959450c0aa196fb8bdcc42ffe3
SeyedehParisaMousaviamiri <parisamsv1993@gmail.com>
2025-06-30 19:26:13 +0200

    change middleware

--- api/src/middlewares/cors.ts
+++ api/src/middlewares/cors.ts
@@ -1,35 +1,32 @@
 import { Request, Response, NextFunction } from 'express'
-
 type CorsOptions = {
-	restrictedOrigin?: boolean
-	origin?: string
+  restrictedOrigin?: boolean
+  origin?: string
 }
-
 export default function cors(options?: CorsOptions) {
-	return (req: Request, res: Response, next: NextFunction) => {
-		if (options?.restrictedOrigin) {
-			if (process.env.NODE_ENV == 'development') {
-				res.header(
-					'Access-Control-Allow-Origin',
-					'http://localhost:3000'
-				)
-			} else {
-				res.header('Access-Control-Allow-Origin', `${options.origin}`)
-			}
-			res.header('Access-Control-Allow-Credentials', 'true')
-		} else {
-			res.header('Access-Control-Allow-Origin', '*')
-		}
-
-		res.header(
-			'Access-Control-Allow-Methods',
-			'GET,PUT,POST,DELETE,OPTIONS'
-		)
-		res.header(
-			'Access-Control-Allow-Headers',
-			'Content-Type, Authorization, X-Requested-With, Content-Length, X-Requested-Width, Accept, Access-Control-Allow-Credentials'
-		)
-		if (req.method === 'OPTIONS') res.sendStatus(200)
-		else next()
-	}
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (options?.restrictedOrigin) {
+      const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001']
+      const origin = req.headers.origin
+      if (origin && allowedOrigins.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', origin)
+      } else {
+        res.header('Access-Control-Allow-Origin', 'null')
+        // return res.status(403).send('CORS policy: Origin not allowed')
+      }
+      res.header('Access-Control-Allow-Credentials', 'true')
+    } else {
+      res.header('Access-Control-Allow-Origin', '*')
+    }
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Authorization, X-Requested-With, Content-Length, X-Requested-Width, Accept, Access-Control-Allow-Credentials'
+    )
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200)
+    } else {
+      next()
+    }
+  }
 }
