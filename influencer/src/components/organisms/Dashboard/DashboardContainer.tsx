2bd2f63b1a8edae1a1d025f95f21369c1ef0a939
SeyedehParisaMousaviamiri <parisamsv1993@gmail.com>
2025-07-12 22:45:58 +0200

    add brand dashboard container

--- influencer/src/components/organisms/Dashboard/DashboardContainer.tsx
+++ influencer/src/components/organisms/Dashboard/DashboardContainer.tsx
@@ -4,45 +4,40 @@ import Loading from "@/components/ui/Loading";
 import DashboardCard from "@/components/organisms/Dashboard/DashboardCard";
 
 function DashboardContainer() {
-  const { user } = useUser();
-  const { cards, loading, error } = useDashboardData(user?.id || '');
+    const { user } = useUser();
+    const { cards, loading, error } = useDashboardData(user?.id ||'');
+
+    const visibleCards = cards?.slice(0, 9);
 
   return (
-    <div className="flex min-h-screen bg-gray-100">
-      <main className="flex-1 mt-[12px] ml-[146px] mr-0 mb-0">
-        <div className="bg-[#E9E9E9] rounded-xl shadow pt-[37px] pl-[34px] pr-[35px] pb-[36px] mx-2 sm:mx-4 md:mx-[30px]">
-          <div className="flex justify-between items-center mb-6 sm:mb-8">
-            <h1 className="text-xl font-bold">Statistics Overview</h1>
-          </div>
-
-      
+    <div className="flex min-h-screen bg-white">
+        <main className="flex-1 px-4 sm:px-6 md:px-8 py-6">
+         <div className="bg-[#E9E9E9] rounded-2xl p-6 sm:p-8 shadow-md">
+             <h1 className="text-base sm:text-lg font-semibold mb-6">Statistics Overview</h1>
 
               {/* ✅ Loading */}
               {loading && (
-                 <div className="flex items-center justify-center min-h-[200px] col-span-full">
+                 <div className="flex items-center justify-center bg-white min-h-[200px] rounded-xl">
                   <Loading color="#FF5500" size="xs" />
                 </div>
               )}
 
               {/* ✅ Error */}
               {!!error && (
-                <div className="text-red-600 text-center p-4 col-span-full">
+                <div className="text-red-600 text-center p-4">
                   {error || 'Something went wrong'}
                 </div>
               )}
 
                {/* ✅ Cards */}
 
-              {cards &&
-                cards.map((card, index) => (
-                   <div
-                    key={index}
-                    className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 sm:gap-6 w-[200px] aspect-[16/10]"
-                  >
-                    <DashboardCard card={card} />
-                  </div>
-                ))
-              }
+               {visibleCards && (
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 sm:gap-6">
+              {visibleCards.map((card, index) => (
+                <DashboardCard key={index} card={card} />
+              ))}
+            </div>
+          )}
         </div>
       </main>
     </div>
